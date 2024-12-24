07f466c36082268c42e76a694891ff30fae72c85
Vishva <adharsh.scipy@gmail.com>
2024-12-24 14:41:06 +0530

    clear

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import cookieParser from "cookie-parser";
 import dotenv from 'dotenv'
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 import adminRouter from './routes/adminRouter.js'
 import userRouter from './routes/userRouter.js';
@@ -12,29 +11,26 @@ import sectionRoute from './routes/sectionRoute.js';
 
 dotenv.config()
 
-//it is for deploy build purpose
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express()
 dotenv.config({
     path: './env'
 })
 
-
-const buildpath = path.join(__dirname, "../client/build")
-app.use(express.static(buildpath));
-app.get("/*", function (req, res) {
-    // app.use(express.static(buildpath));
-    res.sendFile(
-        path.join(buildpath, "../client/build/index.html"),
-        function (err) {
-            if (err) {
-                res.status(500).send(err);
-            }
-        }
-    )
-})
+// //it is for deploy build purpose
+// const _dirname = path.dirname(__filename)
+// const buildpath = path.join(_dirname, "../client/build")
+// app.use(express.static(buildpath));
+// app.get("/*", function (req, res) {
+//     // app.use(express.static(buildpath));
+//     res.sendFile(
+//         path.join(buildpath, "../client/build/index.html"),
+//         function (err) {
+//             if (err) {
+//                 res.status(500).send(err);
+//             }
+//         }
+//     )
+// })
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
